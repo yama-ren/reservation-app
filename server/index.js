5b5072931e3a33eec52e3c4ad8b4056494857653
yama-ren <renren@sanbakaraisu.com>
2021-03-14 01:45:07 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productsRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
   () => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if(process.env.NODE_ENV !== 'production') {
+      const fakeDb = new FakeDb()
+      // fakeDb.initDb()
+    }
   }
 )
 
 const app = express()
 
 app.use('/api/v1/products', productsRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join( __dirname, '..', 'dist', 'reservation-app')
+  app.use(express.static(appPath))
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
+
 
 const PORT = process.env.PORT || '3001'
 
